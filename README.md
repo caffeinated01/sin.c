f800bf81b51a8f6dc928af53a91b06102c8c46c3
Javier <javier.has.no.life@gmail.com>
2025-08-26 20:37:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+## sin.c
+```
+                                                   *****
+                                               ****     ****
+                                             **             **
+*                                          **                 **
+ **                                       *                     *
+   *                                    **                       **
+    **                                **                           *                                 **
+      *                              *                              **                              *
+       **                           *                                 *                           **
+         *                        **                                   **                        *
+          **                    **                                       **                    **
+            **                **                                           *                 **
+              **            **                                              ***            **
+                ***      ***                                                   ***      ***
+                   ******                                                         ******
+```
+a simple program that displays a sin wave within your terminal.\
+run `gcc sin.c -o sin` to build the executable. \
+add a character behind like `./sin '.'` to use `.` instead of the default character which is `*`.
---
+++ sin.c
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+void cleanup(int sig) {
+  printf("\x1b[?25h"); // show cursor
+  fflush(stdout);
+  _exit(0);
+}
+
+int main(int argc, char **argv) {
+  // screen properties
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); // get window size
+
+  int screen_width = w.ws_col;
+  int screen_height = w.ws_row;
+
+  // wave properties
+  float amplitude = screen_height / 4.0f; // scale with window size
+  float frequency = 0.1f;
+  float speed = 0.2f;
+  float offset = 0.0f;
+
+  // buffer
+  char buffer[screen_height * screen_width];
+
+  // default char
+  char pixel = '*';
+
+  if (argc >= 2) {
+    if (argc >= 2) {
+      pixel = argv[1][0]; // set custom char
+    }
+  }
+
+  printf("\x1b[2J");   // clear screen
+  printf("\x1b[?25l"); // hide cursor
+
+  signal(SIGINT, cleanup); // ctrl-c will trigger cleanup
+
+  while (1) {
+    memset(buffer, ' ', screen_width * screen_height);
+
+    // iterate through columns
+    for (int x = 0; x < screen_width; ++x) {
+      float y = amplitude * sinf(frequency * x + offset);
+      int screen_y =
+          (int)(screen_height / 2.0f + y); // shift pivot to center of screen
+
+      if (screen_y >= 0 && screen_y < screen_height) {
+        int idx = x + screen_y * screen_width;
+        buffer[idx] = pixel;
+      }
+    }
+
+    printf("\x1b[H"); // move cursor to top left
+
+    for (int i = 0; i < screen_width * screen_height; i++) {
+      putchar(i % screen_width == 0 ? '\n' : buffer[i]); // wrap to next line
+    }
+
+    offset += speed;
+    usleep(50000);
+  }
+
+  return 0;
+}
